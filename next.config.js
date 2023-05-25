0cd7d0d3d0db138dce3d8afcc608878a54a37a6f
danielvictory <danieljvictory@gmail.com>
2023-05-25 09:26:17 -0500

    add styled components with babel to the boilerplate - remove font and base content from boilerplate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // Change the config file to play nice with styled components
+  // *** Why does this need to happen? Server side rendering? ***
+
+  reactStrictMode: false, // *** Seems wrong to turn this off??? ***
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+      fileName: true,
+      topLevelImportPaths: [],
+      meaninglessFileNames: ["index"], // *** seems hurtful, ha - what's the deal? ***
+      cssProp: true,
+      namespace: "",
+      minify: false,
+      transpileTemplateLiterals: false,
+      pure: false,
+    }
+  }
 }
 
 module.exports = nextConfig
