03d9723293bd1fd1fafa9495aaeb80e1bf63cd58
danielvictory <danieljvictory@gmail.com>
2023-05-25 12:58:21 -0500

    create and add two static image assets that hold position while page shifts using styled components

--- components/Generator/GenElements.tsx
+++ components/Generator/GenElements.tsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 import Link from "next/link";
 import Image from "next/image";
 
+// code "exsc" : export styled component to create basic syntax of below
 export const GradientBackgroundCon = styled.div`
     // Wrapping all styles in a component allows easy access to the rest of the project without having to import each item (eg styles, animation, effects)
 
-    background: linear-gradient(to right, #000046, #1CB5E0);
-    background-size: 400% 400%;
-    animation: gradient 6s ease infinite;
+    background: linear-gradient(to left, #f5d68e, #f2c222, #b85b5b);
+    background-size: 300% 300%;
+    animation: gradient 10s ease infinite;
     height: 100vh;
     width: 100vw;
 
@@ -25,3 +26,37 @@ export const GradientBackgroundCon = styled.div`
     }
 `;
 
+export const BackgroundImage1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  margin-left: 25px;
+  margin-top: 25px;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+  position: fixed;
+  z-index: 1;
+  right: 25px;
+  bottom: 25px;
+`;
+
+export const FooterCont = styled.div`
+    width: 100vw;
+    height: 50px;
+    text-align: center;
+    font-family: 'Source Code Pro', monospace;
+    font-size: 15px;
+    position: absolute;
+    bottom: 0;
+    color: white;
+    z-index: 99999;
+  
+`;
+
+export const RedSpan = styled.span`
+    color: red;
+`;
+
+export const FooterLink = styled(Link)`
+    color: white;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
-import { GradientBackgroundCon } from '@/components/Generator/GenElements'
 import Head from 'next/head'
 import Image from 'next/image'
 
+// React
+import { useState } from 'react'
+
+//Components
+import { GradientBackgroundCon, BackgroundImage1, BackgroundImage2, FooterCont, FooterLink, RedSpan } from '@/components/Generator/GenElements'
+
+// Assets
+import DandayamanaStandingBowPose from '@/assets/DandayamanaStandingBowPose.png'
+import VrksasanaTreePose from '@/assets/VrksasanaTreePose.png'
+
 export default function Home() {
+
+  const [numberOfAsanas, setNumberOfAsanas] = useState(0)
+
   return (
     <>
       <Head>
@@ -13,7 +25,23 @@ export default function Home() {
       </Head>
       {/* Background */}
       <GradientBackgroundCon>
-        
+
+        <BackgroundImage1 src={DandayamanaStandingBowPose}
+        height="250"
+        alt="Dandayamana or Standing Bow Pose" />
+
+        <BackgroundImage2 src={VrksasanaTreePose}
+        height="325"
+        alt="Vrksasana or Tree Pose" />
+
+        <FooterCont>
+          <>
+            Asanas Generated: {numberOfAsanas}
+            <br />
+            Developed with <RedSpan>❤️</RedSpan> by <FooterLink href="#" target="_blank" rel="noopener noreferrer"> Daniel Victory</FooterLink>
+          </>
+        </FooterCont>
+
       </GradientBackgroundCon>
     </>
   )
